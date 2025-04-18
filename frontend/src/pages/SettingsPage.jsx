d1442bf41402d98bb13ecc6452e190c94a6f02dd
BHSajuu <boktiarh_ug_22@mech.nits.ac.in>
2025-04-18 15:10:57 +0530

    add a X icon in setting poage

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,7 +1,7 @@
-import { Send } from "lucide-react";
+import { Send, X } from "lucide-react";
 import { THEMES } from "../constants";
 import { useThemeStore } from "../store/useThemeStore";
-
+import {useNavigate} from "react-router-dom"
 const PREVIEW_MESSAGES = [
   { id: 1, content: "Hey! How's it going?", isSent: false },
   {
@@ -13,16 +13,24 @@ const PREVIEW_MESSAGES = [
 
 const SettingsPage = () => {
   const { theme, setTheme } = useThemeStore();
+     
+  const navigate = useNavigate()
 
   return (
     <div className="h-screen container mx-auto px-4 pt-20 max-w-5xl">
       <div className="space-y-6">
-        <div className="flex flex-col gap-1">
+       
+       <div className="flex items-center justify-between mb-4">
+       <div className="flex flex-col gap-1">
           <h2 className="text-lg font-semibold">Theme</h2>
           <p className="text-sm text-base-content/70">
             Choose a theme for your chat interface
           </p>
         </div>
+
+        <button onClick={() => navigate("/")} className="transition-transform duration-200 hover:scale-120"><X /></button>
+       </div>
+         
 
         <div className="grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 gap-2">
           {THEMES.map((t) => (
