d83ddf176d7450693ce94ef7f7d192a27fc30ce8
BHSajuu <boktiarh_ug_22@mech.nits.ac.in>
2025-06-30 20:59:48 +0530

    Now side bar look like similar to whatsapp in mobile screen

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,10 +10,13 @@ const HomePage = () => {
     <div className="h-screen bg-base-200">
       <div className="flex items-center justify-center pt-20 md:px-12">
         <div className="bg-base-100 rounded-lg shadow-cl w-full  md:max-w-8xl h-[calc(100vh-8rem)]">
-          <div className="flex h-full rounded-lg overflow-hidden">
+          <div className="hidden md:flex h-full rounded-lg overflow-hidden">
             <Sidebar />
 
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+          </div>
+          <div className="md:hidden h-full rounded-lg overflow-hidden">
+            {!selectedUser ? <Sidebar /> : <ChatContainer />}
           </div>
         </div>
       </div>
