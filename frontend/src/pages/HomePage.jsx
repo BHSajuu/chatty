3bffb8f48bbaad7adfa57ef97832d90932b59d25
BHSajuu <boktiarh_ug_22@mech.nits.ac.in>
2025-07-02 18:16:47 +0530

    UI improved

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,10 +7,10 @@ import Sidebar from "../components/Sidebar";
 const HomePage = () => {
   const { selectedUser } = useChatStore();
   return (
-    <div className="h-screen bg-base-200">
+    <div className="h-screen bg-base-200 ">
       <div className="flex items-center justify-center pt-20 md:px-12">
-        <div className="bg-base-100 rounded-lg shadow-cl w-full  md:max-w-8xl h-[calc(100vh-8rem)]">
-          <div className="hidden md:flex h-full rounded-lg overflow-hidden">
+        <div className="bg-base-100 rounded-4xl shadow-cl w-full  md:max-w-8xl h-[calc(100vh-8rem)]">
+          <div className="hidden md:flex h-full rounded-4xl overflow-hidden">
             <Sidebar />
 
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
