c3697328f829af4c813bde6859c54529b0631456
BHSajuu <boktiarh_ug_22@mech.nits.ac.in>
2025-07-02 18:19:38 +0530

    UI improved

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,12 +10,12 @@ const HomePage = () => {
     <div className="h-screen bg-base-200 ">
       <div className="flex items-center justify-center pt-20 md:px-12">
         <div className="bg-base-100 rounded-4xl shadow-cl w-full  md:max-w-8xl h-[calc(100vh-8rem)]">
-          <div className="hidden md:flex h-full rounded-4xl overflow-hidden">
+          <div className="hidden md:flex  h-full rounded-4xl overflow-hidden">
             <Sidebar />
 
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
           </div>
-          <div className="md:hidden h-full rounded-lg overflow-hidden">
+          <div className="md:hidden h-full rounded-4xl overflow-hidden">
             {!selectedUser ? <Sidebar /> : <ChatContainer />}
           </div>
         </div>
